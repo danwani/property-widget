f5a3a07190823f51c1091e9070f992e4347c60b0
danwani <danwani@gmail.com>
2018-10-05 14:59:38 +1000

    Refactored and all handlers working correctly

--- src/components/PropertyPage.js
+++ src/components/PropertyPage.js
@@ -1,19 +1,6 @@
 // Container that holds the Property Dialog
 
 import React, { Component } from "react";
-import { Button } from "@rmwc/button";
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  DialogButton
-} from "@rmwc/dialog";
-
-import "./PropertyPage.css";
-import "@material/button/dist/mdc.button.css";
-import "@material/dialog/dist/mdc.dialog.css";
-import "@material/button/dist/mdc.button.css";
 import PropertyDialog from "./PropertyDialog";
 
 class PropertyPage extends Component {
@@ -26,6 +13,8 @@ class PropertyPage extends Component {
     };
   }
 
+  // handleChange = {property, event} => {this.setState({property: event.target.value});
+
   onPropertyNameChange = event => {
     this.setState({ propertyName: event.target.value });
     // console.log(this.state.propertyName);
@@ -36,49 +25,26 @@ class PropertyPage extends Component {
     // console.log(this.state.propertyNotes);
   };
 
+  onSubmitHandler = evt => this.setState({ standardDialogOpen: true });
+
+  closeDialog = evt => {
+    this.setState({
+      standardDialogOpen: false,
+      propertyName: "",
+      propertyNotes: ""
+    });
+  };
+
   render() {
     return (
       <div>
-        {/** Standard dialog usage */}
-        <Dialog
-          className="property-page"
-          open={this.state.standardDialogOpen}
-          onClose={evt => {
-            console.log(evt.detail.action);
-            this.setState({
-              standardDialogOpen: false,
-              propertyName: "",
-              propertyNotes: ""
-            });
-          }}
-        >
-          <DialogTitle>Property Details</DialogTitle>
-          <DialogContent>
-            <PropertyDialog onPropertyNameChange={this.onPropertyNameChange} onPropertyNotesChange={this.onPropertyNotesChange}/>
-          </DialogContent>
-          <DialogActions>
-            <DialogButton action="close">Add to CRM</DialogButton>
-            <a
-              href={`mailto:info@orbmaps.com?subject=Info for ${
-                this.state.propertyName
-              }&amp;body=${this.state.propertyNotes}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <DialogButton action="accept" isDefaultAction>
-                Prepare mail
-              </DialogButton>
-            </a>
-          </DialogActions>
-        </Dialog>
-
-        <Button
-          className="property-page__call-to-action"
-          raised
-          onClick={evt => this.setState({ standardDialogOpen: true })}
-        >
-          View property details
-        </Button>
+        <PropertyDialog
+          onPropertyNameChange={this.onPropertyNameChange}
+          onPropertyNotesChange={this.onPropertyNotesChange}
+          onSubmitHandler={this.onSubmitHandler}
+          closeDialog={this.closeDialog}
+          state={this.state}
+        />
       </div>
     );
   }
